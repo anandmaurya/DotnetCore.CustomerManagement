be9053c5119e91a1eebc187129ef5f37e25de9ce
anandmaurya <anand.anand0030@gmail.com>
2024-04-13 15:33:48 +0530

    Email service

---
+++ Customer.Management.Infrastrucure/Interface/Email/IEmailService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomerManagement.Infrastrucure.Interface.Email
+{
+    public interface IEmailService
+    {
+
+        Task SendEmail(string receipintEmail, string emailSubject, string emailBody);
+
+    }
+}
